4db69330bd426110a451e1c811407f249a2fcb2e
github username <github email address>
2019-08-07 08:20:16 -0400

    initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This works!!</h1>
+    </div>
+  );
+}
+
+export default App;
